6c011889e97ef1bc9f056e12b7358266c7c21533
Terrambc <S58462@bht-berlin.de>
2022-11-27 00:41:49 +0100

    Finished Version 1.0 * transforming in a better code structure * defining more functions

--- wakandatranslator.py
+++ wakandatranslator.py
@@ -1,65 +1,124 @@
-import tkinter
-from PIL import Image, ImageTk, ImageFont, ImageDraw
-
-# Funktion Clear
-def clear():
-    texteingabe.delete(1.0, tkinter.END)
+import tkinter as tk
+from functools import partial
+from PIL import Image, ImageFont, ImageDraw
 
 
-# Text übersetzen in wakandanisch
-def uebersetzen():
-    eingabe = texteingabe.get(1.0, tkinter.END)
-
-    font = ImageFont.truetype(font='WakandaForever_Regular.ttf', size=50)
-    image = Image.new(mode='RGB', size=(650, 300), color='#9370db')
-    draw = ImageDraw.Draw(im=image)
-    draw.text(xy=(100, 100), text=eingabe, font=font, fill='black', anchor='mm')
-    image.show()
-
-   # img = ImageTk.PhotoImage()
-   # textausgabe = tkinter.Frame(right_frame, image=img, width=650, height=300, bg="MediumPurple1", relief="flat")
-   # textausgabe.grid(row=2, column=1, pady=5, padx=5)
+def clear(textinput, textoutput):
+    """
+    Clear the input and output
+    :param textinput:
+    :param textoutput:
+    :return:
+    """
+    textinput.delete(1.0, tk.END)
+    textoutput.delete(1.0, tk.END)
 
 
-root = tkinter.Tk()
-root.title("Translator Wakanda")
-root.config(bg="gray17")
+def translate(textinput, textoutput):
+    """
+    translate the textinput message to wakandanisch and
+    put call a new function to insert the translated message as an image into the textoutput
+    :param textinput:
+    :param textoutput:
+    :return:
+    """
+    textoutput.delete(1.0, tk.END)
+    message = textinput.get(1.0, tk.END)
+    # get_picture(message)
 
-left_frame = tkinter.Frame(root, width=300, height=600, bg="MediumPurple1")
-left_frame.grid(row=0, column=0, padx=10, pady=5)
+    txt = Image.new(mode='RGB', size=(600, 400), color='#9370db')
+    font = ImageFont.truetype(font='WakandaForever_Regular.ttf', size=20)
+    draw = ImageDraw.Draw(txt)
+    draw.text(xy=(10, 10), text=message, font=font, fill='black')
+    txt.save('message.png')
+    del draw
 
-title_frame = tkinter.Frame(left_frame, width=250, height=50, bg="snow", relief="flat")
-title_frame.grid(row=1, column=0, padx=5, pady=5)
-tkinter.Label(left_frame, text="Übersetzer Wakandanisch", font=("Helvetica", 14, "bold"), background="snow").grid(row=1,
-                                                                                                                  column=0)
-logo = tkinter.PhotoImage(file="wakanda.png")
-logo_frame = tkinter.Frame(left_frame, width=250, height=100, bg="snow", relief="flat")
-logo_frame.grid(row=2, column=0, padx=5, pady=5)
-tkinter.Label(left_frame, image=logo, background="snow").grid(row=2, column=0)
-
-explanation_frame = tkinter.Frame(left_frame, width=250, height=415, bg="snow", relief="flat")
-explanation_frame.grid(row=3, column=0, padx=5, pady=5)
-tkinter.Label(left_frame, text="Hier könnt ihr euren Text in \nWakandisch übersetzen lassen.", font=("Helvetica", 12),
-              background="snow", anchor="n").grid(row=3, column=0, pady=15, sticky='n')
-
-button_frame = tkinter.Frame(left_frame, background="snow")
-button_frame.grid(row=3, column=0)
-
-eingabeButton = tkinter.Button(button_frame, text="Text übersetzen", command=uebersetzen, bg='goldenrod1', width=20)
-eingabeButton.grid(row=3, column=0, pady=5)
-
-clearButton = tkinter.Button(button_frame, text="Eingabe löschen", command=clear, bg='purple3', width=20)
-clearButton.grid(row=4, column=0, pady=5)
+    add_image(textoutput)
 
 
-right_frame = tkinter.Frame(root, width=600, height=600, bg="MediumPurple1")
-right_frame.grid(row=0, column=1, padx=10, pady=5)
+def add_image(textoutput):
+    """
+    insert the translated message as an image to textoutput
+    :param textoutput:
+    :return:
+    """
+    global my_image
+    my_image = tk.PhotoImage(file='message.png')
+    textoutput.image_create(tk.END, image=my_image)
 
-texteingabe = tkinter.Text(right_frame, width=50, height=8, font=("Helvetica", 18))
-texteingabe.grid(row=1, column=1, pady=5, padx=5)
 
-# textausgabe = tkinter.Frame(right_frame, width=650, height=300, bg="MediumPurple1", relief="flat")
-# textausgabe.grid(row=2, column=1, pady=5, padx=5)
+def main():
+    root = tk.Tk()
+    root.title('Translator')
+    root.config(bg="gray17")
+
+    # left side
+    outer_canvas = tk.Frame(root, width=370, height=570, bg='MediumPurple1')
+    outer_canvas.grid(row=0, column=0, sticky=tk.E)
+    outer_canvas.grid_propagate(False)
+    frame = tk.Frame(outer_canvas, bg='snow', width=350, height=560)
+    frame.grid(row=0, column=0, columnspan=2, padx=10, pady=5)
+    frame.grid_propagate(False)
+
+    tk.Label(
+        frame,
+        text='Translator Wakandanisch',
+        font=('Helvetica', 14, 'bold'),
+        background='snow'
+    ).grid(row=1, column=0, padx=10, pady=5)
+
+    logo_image = tk.PhotoImage(file='wakanda.png')
+    tk.Label(frame, image=logo_image, background='snow').grid(
+        row=2, column=0, pady=5, padx=5
+    )
+
+    tk.Label(
+        frame,
+        text="Let you message be translated into wakandisch.",
+        font=('Helvetica', 12),
+        background='snow'
+    ).grid(row=3, column=0, pady=15, padx=5, sticky=tk.NSEW)
+
+    button_frame = tk.Frame(frame, background='snow', height=350)
+    button_frame.grid(row=4, column=0, padx=5, pady=5, sticky=tk.NSEW)
+
+    translate_button = tk.Button(
+        button_frame, text="Translate message", bg='goldenrod1'
+    )
+    translate_button.grid(row=0, column=0, pady=5, padx=5, sticky=tk.NSEW)
+
+    clear_button = tk.Button(
+        button_frame, text="Delete message", bg='purple3'
+    )
+    clear_button.grid(row=0, column=1, padx=5, pady=5, sticky=tk.NSEW)
+
+    # right side
+    frame = tk.Frame(root, bg='MediumPurple1')
+    frame.grid(row=0, column=2, pady=5, padx=10)
+
+    textinput = tk.Text(
+        frame,
+        width=50,
+        height=8,
+        font=('Helvetica', 16),
+        bg='snow'
+    )
+    textinput.grid(row=0, column=0, padx=5, pady=5)
+    textinput.insert(tk.END, "Insert your message here")
+
+    textoutput = tk.Text(
+        frame,
+        width=75,
+        height=22,
+        bg='MediumPurple1'
+    )
+    textoutput.grid(row=2, column=0, padx=5, pady=5)
+
+    translate_button["command"] = partial(translate, textinput, textoutput)
+    clear_button["command"] = partial(clear, textinput, textoutput)
+
+    root.mainloop()
 
 
-root.mainloop()
+if __name__ == '__main__':
+    main()
